21eb58cb1759e4de4849fbf802bcbbdd2973fb49
Court Jansma <courtjansma33@gmail.com>
2026-06-15 09:47:55 -0500

    Canonical social profile URLs

---
+++ src/sincor2/social_links.py
@@ -0,0 +1,29 @@
+"""Canonical SINCOR social profile URLs."""
+
+SOCIAL_LINKS = {
+    "x": {
+        "label": "X",
+        "url": "https://x.com/getsincor",
+        "alt": "Follow SINCOR on X",
+    },
+    "telegram": {
+        "label": "Telegram",
+        "url": "https://t.me/getsincor",
+        "alt": "Join SINCOR on Telegram",
+    },
+    "discord": {
+        "label": "Discord",
+        "url": "https://discord.gg/getsincor",
+        "alt": "Join the SINCOR Discord",
+    },
+    "facebook": {
+        "label": "Facebook",
+        "url": "https://facebook.com/getsincor",
+        "alt": "Follow SINCOR on Facebook",
+    },
+    "farcaster": {
+        "label": "Farcaster",
+        "url": "https://warpcast.com/getsincor",
+        "alt": "Follow SINCOR on Farcaster",
+    },
+}
